69fd8dcb182ea72d03cb0e86cbe5b043e3c08b2c
Jordan Sivils <jordan.sivils.wd@gmail.com>
2023-11-08 19:43:28 -0500

    Removed Contact, and Templated About

--- controller/aboutAndContactController.js
+++ controller/aboutAndContactController.js
@@ -2,6 +2,3 @@ module.exports.get_about = (req, res) => {
     res.render('about', {title: 'About'});
 }
 
-module.exports.get_contact = (req, res) => {
-    res.render('contact', {title: 'Contact'})
-}
--- routes/aboutAndContactRoutes.js
+++ routes/aboutAndContactRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const getAboutAndContact = require('../controller/aboutAndContactController');
 
-router.get('/contact', getAboutAndContact.get_contact);
+
 router.get('/about', getAboutAndContact.get_about);
 
 module.exports = router
